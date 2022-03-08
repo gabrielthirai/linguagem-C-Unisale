0161446f2ffdf0d96e2207ea4641230eb9ffbce4
GabrielTHirai <gabrielt.hirai@gmail.com>
2022-03-07 21:37:50 -0300

    atualizacao

---
+++ aula07-03-22pt2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+main(){
+    int numfunc, qtdfilhos;
+    float numh, vlrh, salfunc, acres, salfinal;
+    cout<<"Informe o numero de funcionarios ";
+    cin>>numfunc;
+    cout<<"Informe o numero de horas trabalhadas ";
+    cin>>numh;
+    cout<<"Informe o valor da hora trabalhada ";
+    cin>>vlrh;
+    cout<<"Informe a quantidade de filhos menores de 14 anos";
+    cin>>qtdfilhos;
+    salfunc=(numh*vlrh);
+    acres=(salfunc*0.10) * qtdfilhos;
+    salfinal=(salfunc+acres);
+    cout<<salfinal;
+}
